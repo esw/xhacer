72876430b1d590b51919903ec333a38f379e590d
Jose Luis Caro <jlcaro@Jose-Luis-Caros-MacBook-Pro.local>
2010-06-14 10:35:51 -0500

    Modifies base.html template to link to the url names of login and logout modifies main urls.py to access admin without an ending '/'

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/(.*)',admin.site.root),
+    url(r'^admin(.*)',admin.site.root),
     url(r'^login$', 'django.contrib.auth.views.login', name="acct_login"),
     url(r'^logout$', 'django.contrib.auth.views.logout', name="acct_logout"),
     url(r'^', include('places.urls')),
